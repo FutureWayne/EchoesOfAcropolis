b0627c5541c9b4e3219325e92333b76758c5441e
Future Wayne <futurewayne.ludens@gmail.com>
2024-01-09 11:48:00 -0700

    #1 Initial Commit

---
+++ Source/EchoesOfAcropolis/EchoesOfAcropolisGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EchoesOfAcropolisGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AEchoesOfAcropolisGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AEchoesOfAcropolisGameMode();
+};
+
+
+
