cc619075ade13d9550a1c518363e0b1f8f4f11b4
Future Wayne <futurewayne.ludens@gmail.com>
2024-01-09 17:37:13 -0700

    #3 Integrate Ninja Character Plugin
    Subclass from ninja character, supporting directional gravity

---
+++ Source/EchoesOfAcropolis/Public/Character/EchoCharacterBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NinjaCharacter.h"
+#include "EchoCharacterBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ECHOESOFACROPOLIS_API AEchoCharacterBase : public ANinjaCharacter
+{
+	GENERATED_BODY()
+
+public:
+	AEchoCharacterBase(const FObjectInitializer& ObjectInitializer);
+	
+};
